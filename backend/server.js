edd66de8af07beaccfe66bd58f450d64a834a6d4
Juand12348 <156850435+Juand12348@users.noreply.github.com>
2025-11-06 20:27:46 -0500

    Refactor server.js for improved clarity

--- backend/server.js
+++ backend/server.js
@@ -5,20 +5,18 @@ const usuariosRoutes = require("./routes/usuarios");
 const app = express();
 const PORT = 5001;
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// Rutas principales
-// 👇 Esto hace que las rutas dentro de usuarios.js se monten en /api
+
 app.use("/api", usuariosRoutes);
 
-// Ruta de prueba
+
 app.get("/", (req, res) => {
-  res.json({ message: "🚀 API de Usuarios funcionando correctamente" });
+  res.json({ message: "API de Usuarios funcionando correctamente" });
 });
 
-// Servidor escuchando
 app.listen(PORT, () => {
-  console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
